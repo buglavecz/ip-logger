1513d028d5db91365bd5f9e8d5b6df5ac8bdfce7
Peter Buglavecz <buggio.dev@gmail.com>
2025-02-10 22:24:57 +0100

    feat: init nuxt app

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+  // const { data } = await useFetch('/api/data')
+  const { body } = await $fetch('/api/log', {
+    method: 'post',
+    body: { test: 123 }
+  })
+</script>
+
+<template>
+<h1>welcome</h1>
+<h3>{{ body }}</h3>
+</template>
+
+<style scoped>
+
+</style>
