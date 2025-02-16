13674e6902294b6ce581deec936c5dd179b6ada6
Peter Buglavecz <buggio.dev@gmail.com>
2025-02-16 19:07:06 +0100

    ✨ feat: log data + list data

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+const DEFAULT_DB_SCHEMA = 'prod';
 export default defineNuxtConfig({
 	modules: [
 		'@nuxt/eslint',
@@ -6,8 +7,9 @@ export default defineNuxtConfig({
 	devtools: { enabled: true },
 	runtimeConfig: {
 		public: {
-			supabaseUrl: process.env.SUPABASE_URL,
-			supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
+			SBUrl: process.env.SUPABASE_URL,
+			SBAnonKey: process.env.SUPABASE_ANON_KEY,
+			SBSchema: process.env.SUPABASE_SCHEMA || DEFAULT_DB_SCHEMA,
 		},
 	},
 	compatibilityDate: '2024-11-01',
--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,55 @@
 <script setup lang="ts">
+import { FetchError } from 'ofetch';
+
 const { $supabase } = useNuxtApp();
-const { data: rows, error } = await $supabase.from('users').select('*');
+const { data: rows, error } = await $supabase
+	.from('visitors')
+	.select('*')
+	.order('id', { ascending: false });
+
+try {
+	await useFetch('/api/log-visitor', {
+		method: 'POST',
+	});
+}
+catch (e: unknown) {
+	if (e instanceof FetchError) {
+		console.error(e.data.message);
+	}
+}
 </script>
 
 <template>
 	<div>
-		<h1>Hello</h1>
+		<h1>Visitors IPs</h1>
 
-		<div v-if="rows">
-			<ol>
-				<li
-					v-for="user in rows"
-					:key="user.id"
-				>
-					{{ user.last_name }} {{ user.first_name }}
-				</li>
-			</ol>
+		<pre v-if="error">
+      {{ error }}
+    </pre>
+		<div v-else-if="rows">
+			<table
+				style="border-collapse: collapse;border: 1px solid #000;"
+				cellpadding="8"
+				border="1"
+			>
+				<thead>
+					<tr>
+						<th>Date</th>
+						<th>IP</th>
+						<th>User Agent</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr
+						v-for="visitor in rows"
+						:key="visitor.id"
+					>
+						<td>{{ visitor.created_at }}</td>
+						<td>{{ visitor.ip }}</td>
+						<td>{{ visitor.user_agent }}</td>
+					</tr>
+				</tbody>
+			</table>
 		</div>
 	</div>
 </template>
-
-<style scoped>
-
-</style>
